0ecd67a196b57759c40c6be848203080e3b5e39f
Molly-Burke <mollis92@hotmail.com>
2024-06-26 22:54:34 +0200

    changed name

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='seg-quant-ZF-isv',
+    name='seg_quant_ZF_isv',
     version='0.1',
     packages=find_packages(),
     install_requires=[
