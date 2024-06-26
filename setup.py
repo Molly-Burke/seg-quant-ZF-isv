f109b0c697e5ce28ea6fc83a850b3afe612aa710
Molly-Burke <mollis92@hotmail.com>
2024-06-26 22:39:42 +0200

    added code

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='seg-quant-ZF-isv',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        "torch"
+    ],
+    author='Molly Burke',
+    author_email='your.email@example.com',
+    description='A description of your library',
+    url='https://github.com/Molly-Burke/seg-quant-ZF-isv',
+)
